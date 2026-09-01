b1cfaa4e4a90454c8221bdaaa0c872f9e6da3aa3
KristianAgungN <xtian.agung.2020@gmail.com>
2026-09-01 19:10:00 +0700

    Add files via upload

---
+++ Tianproject1.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tianproject1;
+
+/**
+ *
+ * @author xtian
+ */
+public class Tianproject1 {
+
+    public static void main(String[] args) {
+         System.out.println("Nama: Kristian Agung Nugroho "
+                + "\nNPM: 2505060044 "
+                + "\nAlamat: Kota Magelang "
+                + "\nNo HP: 082123904020");
+    }
+}
